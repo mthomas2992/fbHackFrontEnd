98cfa7e5100a811078a04177c3824a05c9df6ef8
Matthew Thomas <matt@afternight.io>
2017-04-30 06:40:45 +1000

    added basic analytics

--- imports/missionAnalytics.jsx
+++ imports/missionAnalytics.jsx
@@ -25,10 +25,30 @@ class MissionAnalytics extends React.Component {
     }
     render() {
       if (this.state.missionStepDetails){
+        console.log(this.state.missionDetails);
         var resultPages= new Array();
-        // for (k=0;k<this.state.missionStepDetails.length;k++){
-        //
-        // }
+        for (k=0;k<this.state.missionDetails.steps.length;k++){
+          var elementToBePushed = new Array();
+          for (j=0;j<this.state.missionStepDetails.length;j++){
+            if (this.state.missionStepDetails[j].step == this.state.missionDetails.steps[k]){
+              if (this.state.missionStepDetails[j].stepInfo.type == "camera"){
+                elementToBePushed.push(<div className= "col-xs-12" id= "imgDivRoot"><img id="imgDisp" src={this.state.missionStepDetails[j].content}></img></div>)
+              } else if (this.state.missionStepDetails[j].stepInfo.type == "shortQ"){
+                elementToBePushed.push(this.state.missionStepDetails[j].content)
+              }
+
+            }
+          }
+          resultPages.push(<div id="rootStepAnalytics" className="row">
+                            <div className = "col-xs-12">
+                              Step: {k+1} , {this.state.missionStepDetails[k].stepInfo.name}
+                            </div>
+                            {elementToBePushed}
+                          </div>);
+        }
+        if (resultPages.length==0){
+          resultPages=<div>No Results yet!</div>;
+        }
         return (<div className="row" >
                   <div className = "col-xs-12" id = "topMissionAnalyticsHeader">
                     Mission analytics for {this.props.missionActive}
